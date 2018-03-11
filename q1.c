b85df334949c4048c483e2db049793321fd319ce
Clanger72 <langer.langer72@gmail.com>
2018-03-11 19:44:29 -0300

    Add project

--- q1.c
+++ q1.c
@@ -3,8 +3,6 @@
 #include "inserirDadosq2.c"
 #include "imprimirDadosq3.cpp"
 
-/* . Construa um programa que define uma estrutura de tipo pessoa. */
-
 int main(void) {
 	imprime();
 	return 0;
--- q4.cpp
+++ q4.cpp
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "q4.h"
-/*Construa um programa que define um tipo novo de dados, para uma estrutura de um animal, deverá conter atributos como:
- raça, cor, peso e sexo.  E solicite ao usuário do programa 10 espécies e depois imprima em tela. */
- 
  
 int main (void){
  	Animal a[3];
